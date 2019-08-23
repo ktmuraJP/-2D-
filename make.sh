7ed651870c8f0c3e90d32c10373df5f4bf718566
Keita Murakami <36421890+ktmuraJP@users.noreply.github.com>
2019-08-23 11:24:59 -0400

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,12 +1,12 @@
 SOURCE="./src/main.cpp"
 EXEC="2dengine"
 g++ $SOURCE -g -Wall\
-    -I./lib/stb\
-    -I./lib/gl3w\
+    -I./stb\
+    -I./gl3w\
     -I./src/\
     ./src/fileio.cpp\
     ./src/texture.cpp\
-    ./lib/gl3w/GL/gl3w.c\
+    ./gl3w/GL/gl3w.c\
     -o $EXEC -lm -lGL\
     `pkg-config --static --libs glfw3`\
     `pkg-config --cflags --libs freetype2`\
